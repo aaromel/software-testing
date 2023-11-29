924c8ce48ad149aefb4aeb7c33a5232b077e5692
Aaro Melchy <aaro.melchy@tuni.fi>
2023-11-29 20:43:20 +0200

    Implement endsWith.js test

---
+++ test/endsWith.test.js
@@ -0,0 +1,41 @@
+import chai from "chai";
+const expect = chai.expect;
+
+import endsWith from "../src/endsWith.js";
+
+describe("endsWith", () => {
+  it("should return true if string ends with the target string", () => {
+    const result = endsWith("abc", "c");
+    expect(result).to.be.true;
+  });
+
+  it("should return false if string does not end with the target string", () => {
+    const result = endsWith("abc", "b");
+    expect(result).to.be.false;
+  });
+
+  it("should return true when specifying a position within the string", () => {
+    const result = endsWith("abc", "b", 2);
+    expect(result).to.be.true;
+  });
+
+  it("should return false when specifying a position outside the string", () => {
+    const result = endsWith("abc", "b", 1);
+    expect(result).to.be.false;
+  });
+
+  it("should return true when target is an empty string", () => {
+    const result = endsWith("abc", "");
+    expect(result).to.be.true;
+  });
+
+  it("should handle position as a negative number", () => {
+    const result = endsWith("abc", "a", -1);
+    expect(result).to.be.false;
+  });
+
+  it("should handle other than string parameters", () => {
+    const result = endsWith(234, 4);
+    expect(result).to.be.false;
+  });
+});
